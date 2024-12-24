c5bd6f3347b548d25f47b7a499885fbab259e18d
315 <315@315-38>
2024-12-24 10:31:18 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Student
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private double ocenka;
+        public Student(string name, double ocenka)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Ocenka = ocenka;
+        }
+        public double Ocenka
+        {
+            get { return ocenka; }
+            set { ocenka = value; }
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Имя{Name},Оценка{Ocenka}");
+        }
+    }
+}
+public class Group
+{
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public string College;
+    public List<Student> students = new List<Student>();
+    public void ShowAll()
+    {
+        Console.WriteLine($" Название группы{Name}," + $" Колледж {College}\n Список студентов:");
+        foreach (Student student in students)
+        {
+            student.Show();
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Артём", 5);
+            Student student1 = new Student("Сергей", 4.8);
+            Student student2 = new Student("Женя", 4.3);
+            Student student3 = new Student("Миша", 3.1);
+            
+            Group group = new Group();
+            group.Name = "ИС31";
+            group.College = "Мок 1-й";
+
+            group.students.Add(student);
+            group.students.Add(student1);
+            group.students.Add(student2);
+            group.students.Add(student3);
+            group.ShowAll();
+            Console.ReadLine();
+
+        }
+    }
+}
